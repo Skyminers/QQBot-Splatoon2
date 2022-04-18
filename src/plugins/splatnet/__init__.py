8f68533c1f1bea86eec0e3aef1d3ea773ce9f49f
Sky_miner <458482582@qq.com>
2022-04-19 02:30:07 +0800

    更新可选地图查询功能

--- src/plugins/splatnet/__init__.py
+++ src/plugins/splatnet/__init__.py
@@ -1,12 +1,12 @@
 # import nonebot
 from nonebot import get_driver
-from nonebot import on_command
+from nonebot import on_command, on_regex
 from nonebot.typing import T_State
 from nonebot.adapters import Event
 from nonebot.adapters.cqhttp import Bot, MessageSegment, exception
 from .config import Config
 from nonebot_guild_patch import GuildMessageEvent
-from .data_source import get_stage_info, random_image, get_coop_info
+from .data_source import *
 from .utils import *
 from .imageProcesser import *
 
@@ -15,7 +15,28 @@ config = Config(**global_config.dict())
 
 # 群
 
+matcher_select_stage = on_regex('[0-9]+图')
 matcher_kill = on_command('伪确')
+matcher_coop = on_command('工')
+matcher_pool = on_command('开泉')
+matcher_stage_group = on_command('图')
+matcher_stage_group2 = on_command('图图')
+matcher_stage_next1 = on_command('下图')
+matcher_stage_next12 = on_command('下图图')
+matcher_random = on_command('色图')
+
+
+@matcher_select_stage.handle()
+async def _(bot: Bot, event: Event):
+    msg = [int(x) for x in event.get_message().extract_plain_text()[:-1]]
+    msg.sort()
+    img = get_stage_info(msg)
+    await matcher_coop.send(
+        MessageSegment.image(
+            file=img,
+            cache=False,
+        )
+    )
 
 
 @matcher_kill.handle()
@@ -27,8 +48,6 @@ async def _(bot: Bot, event: Event):
         )
     )
 
-matcher_coop = on_command('工')
-
 
 @matcher_coop.handle()
 async def _(bot: Bot, event: Event):
@@ -37,7 +56,16 @@ async def _(bot: Bot, event: Event):
         res
     )
 
-matcher_stage_group = on_command('图')
+
+@matcher_pool.handle()
+async def _(bot: Bot, event: Event):
+    res = get_file(get_info(get_coop_schedule()['details'][0])['stage'] + ' Pool', format_name='jpg')
+    await matcher_pool.send(
+        MessageSegment.image(
+            file=image_to_base64(res),
+            cache=False
+        )
+    )
 
 
 @matcher_stage_group.handle()
@@ -51,9 +79,6 @@ async def _(bot: Bot, event: Event):
     )
 
 
-matcher_stage_group2 = on_command('图图')
-
-
 @matcher_stage_group2.handle()
 async def _(bot: Bot, event: Event):
     img = get_stage_info([0, 1])
@@ -63,9 +88,6 @@ async def _(bot: Bot, event: Event):
             cache=False,
         )
     )
-
-
-matcher_stage_next1 = on_command('下图')
 
 
 @matcher_stage_next1.handle()
@@ -78,8 +100,6 @@ async def _(bot: Bot, event: Event):
             cache=False,
         )
     )
-
-matcher_stage_next12 = on_command('下图图')
 
 
 @matcher_stage_next12.handle()
@@ -94,48 +114,17 @@ async def _(bot: Bot, event: Event):
     )
 
 
-matcher_stage_next2 = on_command('下下图')
-
-
-@matcher_stage_next2.handle()
-async def _(bot: Bot, event: Event):
-    args = str(event.get_message()).strip()
-    img = get_stage_info([2])
-    await matcher_stage_next2.send(
-        MessageSegment.image(
-            file=img,
-            cache=False,
-        )
-    )
-
-
-matcher_stage_next3 = on_command('下下下图')
-
-
-@matcher_stage_next3.handle()
-async def _(bot: Bot, event: Event):
-    args = str(event.get_message()).strip()
-    img = get_stage_info([3])
-    await matcher_stage_next3.send(
-        MessageSegment.image(
-            file=img,
-            cache=False,
-        )
-    )
-
-
 # 随机图片
-matcher_random = on_command('色图')
 
 
 @matcher_random.handle()
 async def _(bot: Bot, event: Event):
     group_id = event.get_session_id().split('_')[1]
-    if check_group_id(group_id):
-        send_msg = MessageSegment.image(file=random_image(), cache=False)
-    else:
-        send_msg = '几点啦，还不睡觉，等着猝死吧你们！'
     try:
+        if check_group_id(group_id):
+            send_msg = MessageSegment.image(file=random_image(), cache=False, timeout=10)
+        else:
+            send_msg = '几点啦，还不睡觉，等着猝死吧你们！'
         await matcher_random.send(send_msg)
     except exception.NetworkError:
         await matcher_random.send(
@@ -145,25 +134,10 @@ async def _(bot: Bot, event: Event):
 
 # 频道
 
-# 获取所有地图
-
-matcher_all_stage = on_command('所有图图')
-
-
-@matcher_all_stage.handle()
-async def _(bot: Bot, event: GuildMessageEvent):
-    for i in range(6):
-        img = get_stage_info([i])
-        await matcher_all_stage.send(
-            MessageSegment.image(
-                file=img,
-                cache=False,
-            )
-        )
-
-
 # 获取地图
 matcher_stage = on_command('图')
+matcher_stage_next = on_command('下图')
+matcher_repeat = on_command('复读')
 
 
 @matcher_stage.handle()
@@ -177,9 +151,6 @@ async def _(bot: Bot, event: GuildMessageEvent):
     )
 
 
-matcher_stage_next = on_command('下图')
-
-
 @matcher_stage_next.handle()
 async def _(bot: Bot, event: GuildMessageEvent):
     args = str(event.get_message()).strip()
@@ -190,11 +161,6 @@ async def _(bot: Bot, event: GuildMessageEvent):
             cache=False,
         )
     )
-
-
-
-# 复读机功能
-matcher_repeat = on_command('复读')
 
 
 @matcher_repeat.handle()
